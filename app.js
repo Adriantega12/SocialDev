e5aa60eab0ecd9173a45250e1aee76039fea6f12
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-11-29 11:30:17 -0600

    Moved test to router. It works.

--- app.js
+++ app.js
@@ -9,28 +9,9 @@ app.engine('mustache', mustacheExpress());
 
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/app/views');
-// app.engine('mustache', mustache(__dirname + '/app/views/partials', '.mustache'));
 app.use(express.static(__dirname + '/public'));
 
 // Cargar rutas
-// app.use(router);
-
-app.get('/', (req, res) => {
-  const locals = {
-
-  };
-  res.render('search', {
-    posts: [ {
-      title: 'Title',
-    },
-    {
-      title: 'Title',
-    },
-    {
-      title: 'Title',
-    },
-    ],
-  });
-});
+app.use(router);
 
 app.listen(process.env.PORT, () => console.log(`Started listening on port ${process.env.PORT}!`));
