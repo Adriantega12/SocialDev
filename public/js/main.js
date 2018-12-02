adfcd83b9c437a1b411b0122caddb7d8ec94e2a3
Adrián Octavio Terrazas García <tavo_terrazasg@hotmail.com>
2018-12-02 14:51:57 -0600

    Add javascript to aid edit functionality of comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+var editCommentBttns = document.querySelectorAll('.edit-comment');
+editCommentBttns.forEach(function (btn) {
+  return btn.addEventListener('click', function () {
+    var editForm = document.getElementById("edit".concat(btn.dataset.id));
+    editForm.classList.toggle('inactive');
+  });
+});
